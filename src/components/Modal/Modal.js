7aeb00e50fb8de39c55e5a301f911eb987a27745
anya.shan <anya.shanaida@yahoo.com>
2022-10-20 20:38:38 +0300

    add css Modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,17 @@
+// import PropTypes from 'prop-types';
+import { Overlay, ModalImage } from './Modal.styled';
+
+export const Modal = () => {
+  return (
+    <Overlay>
+      <ModalImage>
+        <img src="" alt="" />
+      </ModalImage>
+    </Overlay>
+  );
+};
+
+// Section.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   children: PropTypes.node.isRequired,
+// };
