993283bde8c50a79a137aeb672513d7baecbb9e6
anya.shan <anya.shanaida@yahoo.com>
2022-10-20 20:19:03 +0300

    add css ImageGallery

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 // import { Component } from 'react';
 import { Container } from 'components/App.styled';
 import { Searchbar } from 'components/Searchbar/Searchbar';
+import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 
 export const App = () => {
   return (
     <Container>
       <Searchbar />
+      <ImageGallery />
     </Container>
   );
 };
---
+++ src/components/ImageGallery/ImageGallery.js
@@ -0,0 +1,11 @@
+// import PropTypes from 'prop-types';
+import { List } from './ImageGallery.styled';
+
+export const ImageGallery = () => {
+  return <List></List>;
+};
+
+// Section.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   children: PropTypes.node.isRequired,
+// };
