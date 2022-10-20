aca05b072d2bc00bcab888590933a6a2a4e5dec7
anya.shan <anya.shanaida@yahoo.com>
2022-10-20 21:49:12 +0300

    add Pixabay

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,28 @@
-// import { Component } from 'react';
+import { Component } from 'react';
 import { Container } from 'components/App.styled';
 import { Searchbar } from 'components/Searchbar/Searchbar';
-import { ImageGallery } from 'components/ImageGallery/ImageGallery';
+// import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 
-export const App = () => {
-  return (
-    <Container>
-      <Searchbar />
-      <ImageGallery />
-    </Container>
-  );
-};
+export class App extends Component {
+  state = {
+    image: [],
+  };
+
+  componentDidMount() {
+    fetch(
+      'https://pixabay.com/api/?q=cat&page=1&key=29626479-30d098b137805aefe019417a9&image_type=photo&orientation=horizontal&per_page=12'
+    )
+      .then(res => res.json())
+      .then(image => this.setState({ image }));
+  }
+
+  render() {
+    return (
+      <Container>
+        <Searchbar />
+        {this.state.image && <div>результат запиту</div>}
+        {/* <ImageGallery /> */}
+      </Container>
+    );
+  }
+}
