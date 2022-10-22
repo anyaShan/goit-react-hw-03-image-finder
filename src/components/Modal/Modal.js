e883f07d69087557f1f86d7543f8d86fdd8e4cd8
anya.shan <anya.shanaida@yahoo.com>
2022-10-22 21:49:39 +0300

    do Portal for component Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,15 +1,44 @@
+import { Component } from 'react';
+import { createPortal } from 'react-dom';
+
 // import PropTypes from 'prop-types';
 import { Overlay, ModalImage } from './Modal.styled';
 
-export const Modal = ({ other, largeImageLink, closeModal }) => {
-  return (
-    <Overlay onClick={closeModal}>
-      <ModalImage>
-        <img src={largeImageLink} alt={other} />
-      </ModalImage>
-    </Overlay>
-  );
-};
+const modalRoot = document.querySelector('#modal-root');
+
+export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.closeModal();
+    }
+  };
+
+  handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      this.props.closeModal();
+    }
+  };
+
+  render() {
+    const { other, largeImageLink } = this.props;
+    return createPortal(
+      <Overlay onClick={this.handleBackdropClick}>
+        <ModalImage>
+          <img src={largeImageLink} alt={other} />
+        </ModalImage>
+      </Overlay>,
+      modalRoot
+    );
+  }
+}
 
 // Section.propTypes = {
 //   title: PropTypes.string.isRequired,
